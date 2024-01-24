fbe156f96d8cf62a91439a328973e19e10d57d02
João <jp_ascampos@hotmail.com>
2024-01-24 10:24:52 -0300

    Delete Mediator.h

--- src/headers/Mediator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-class BaseComponent;
-class Mediator {
-public:
-	virtual void Notify(BaseComponent* sender, std::string event) const = 0;
-};
